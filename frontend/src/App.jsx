297e59c57dfb5b3b81c4d8ed3b4847d896f20245
S.Manoj Kumar <manojkumarsanda45@gmail.com>
2025-09-11 15:19:38 +0530

    updated profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import LoginPage from './Pages/LoginPage/LoginPage';
-import Profile from './Pages/Profile/profile';
+import Profile from './Pages/Profile/Profile';
 import './index.css';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
